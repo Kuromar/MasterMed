d577ad2b067ffdfe00a3f481edacd4ef74b20cfb
Kuromar <tolstove@inbox.ru>
2017-12-18 13:14:03 +0500

    Applet 1

---
+++ MyApplet/src/HelloWorldApplet.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.applet.*;
+import java.awt.*;
+/**
+ *
+ * @author ZHORA
+ */
+public class HelloWorldApplet extends Applet {
+    public void paint(Graphics g)
+    {
+    g.drawString("Hello World!", 60, 20);
+    }
+   
+}
